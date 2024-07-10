60a1f8ce9f0b6430da05bfb161b94c02f8adbc22
LuBruck <bruck@alunos.utfpr.edu.br>
2024-07-10 15:41:54 -0300

    waa

--- Aula_Mixing/log.py
+++ Aula_Mixing/log.py
@@ -1,10 +1,33 @@
 #Abstração
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / 'log.txt'
+
 class Log:
-    def log(self, msg):
-        print(__name__)
+    def _log(self, msg):
         raise NotImplementedError('Implemente o método log')
 
+    def log_error(self, msg):
+        return self._log(f"Error: {msg} {self.__class__.__name__}")
+        
+    def log_succes(self, msg):
+        return self._log(f"Succes: {msg} {self.__class__.__name__}")
 
-if __name__ == '__main__':
-    l = Log()
-    l.log('penis')
+
+class LogPrint(Log):
+    def _log(self, msg):  #se eu tirar esses 2, e chamar ._log em um Logprint, vai dar o raise NotImplemetedError
+        print(msg)
+
+class LogFile(Log):
+    def _log(self, msg):
+        msg_formatada = f'{msg} ({self.__class__.__name__}) '
+
+        with open(LOG_FILE , 'a') as arquivo:
+            arquivo.write(f'{msg_formatada}\n')
+
+lp = LogPrint()
+lp.log_error("aiiiiii calica")
+
+lf = LogFile()
+lf.log_succes("QUIDILICAA")
+# lf._log('UIUIUIUI')
